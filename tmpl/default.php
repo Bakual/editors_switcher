0a0445048fc612be9b8ed2445e0c9f09804ecd73
Thomas Hunziker <werbemails@bakual.ch>
2021-04-06 20:39:39 +0200

    Finish moving Javascript away from inline code

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,37 +11,54 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\Language\Text;
 
-$confirmation = $this->params->get('confirmation', 1);
 $currentEditor = $this->switcherEditor->_name;
+$confirmation  = $this->params->get('confirmation', 1);
 
-// onchange
-$jsOnchange = 'onchange="';
+// Cookie parameters
+$domain  = $this->app->get('cookie_domain', '');
+$path    = $this->app->get('cookie_path', '/');
+$expires = gmdate('r', time() + $this->params->get('cookie_days', 365) * 24 * 60 * 60);
+
+// Load language string for JS
 if ($confirmation)
 {
 	$this->loadLanguage('plg_editors_switcher', JPATH_PLUGINS . '/editors/switcher');
-
-	// Load language string for JS
 	Text::script('PLG_EDITORS_SWITCHER_CONFIRM_MESSAGE');
-
-	$jsOnchange .= 'if(this.value != \'' . $currentEditor . '\' && confirm(Joomla.JText._(\'PLG_EDITORS_SWITCHER_CONFIRM_MESSAGE\'))){';
 }
 
-// Set cookie to new value
-$domain     = $this->app->get('cookie_domain', '');
-$path       = $this->app->get('cookie_path', '/');
-$expires    = gmdate('r', time() + $this->params->get('cookie_days', 365) * 24 * 60 * 60);
-$jsOnchange .= 'document.cookie = \'' . $this->cookieName . '=\'+this.value+\';domain=' . $domain . ';path=' . $path . ';expires=' . $expires . '\';';
+$javascript = "document.addEventListener('DOMContentLoaded', function(){
+	let elements = document.getElementsByClassName('switcher-selector');
+	let changeEditor = function () {
+		let attribute = this.getAttribute('data-confirmation');
+		if(this.value != '" . $currentEditor . "' && (attribute == 0 || confirm(Joomla.JText._('PLG_EDITORS_SWITCHER_CONFIRM_MESSAGE')))){
+			document.cookie = '" . $this->cookieName . "='+this.value+';domain=" . $domain . ";path=" . $path . ";expires=" . $expires . "';
+			window.location.reload();
+		} else {
+			this.value = '" . $currentEditor . "';
+			if (window.jQuery && jQuery(this).data('chosen')){
+				jQuery(this).trigger('liszt:updated');
+			}
+		}
+	};
+	Array.from(elements).forEach(function (element) {
+		element.addEventListener('change', changeEditor);
+		if (window.jQuery && jQuery(element).data('chosen')){
+			jQuery(element).on('change', changeEditor);
+		}
+	});
+})";
+$doc        = $this->app->getDocument();
+$doc->addScriptDeclaration($javascript);
 
-// Reload page
-$jsOnchange .= 'window.location.reload();';
-
-if ($confirmation)
-{
-	$jsOnchange .= '} else {this.value = \'' . $currentEditor . '\';jQuery(this).trigger(\'liszt:updated\');}';
-}
-$jsOnchange .= '"';
+// Select Options
+$options = array(
+		'list.attr'   => array(
+				'class'             => 'switcher-selector',
+				'data-confirmation' => $confirmation,
+		),
+		'list.select' => $currentEditor,
+);
 ?>
-<div id="switcherSelector" class="btn-toolbar pull-right" style="margin-right:5px;">
-	<?php echo JHtml::_('select.genericlist', $this->editors, 'switcheditor' . ''
-			, $jsOnchange, 'value', 'text', $currentEditor, 'jswitcheditor'); ?>
+<div class="switcher-selector-wrapper btn-toolbar pull-right" style="margin-right:5px;">
+	<?php echo JHtml::_('select.genericlist', $this->editors, 'switcheditor', $options); ?>
 </div>
